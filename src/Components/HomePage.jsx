094c1a9e09529ff43ffcbbd115c3128e08d172bd
AlaaZaiter2 <zaiteralaa29@gmail.com>
2023-12-06 11:06:05 +0200

    new form about

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,6 +1,10 @@
+import BestSaller from "./BestSaller.jsx";
+import Aboutus from "./AboutUs.jsx";
 function HomePage() {
     return (
       <div>
+        <BestSaller/>
+        <Aboutus/>
       </div>
     );
   }
