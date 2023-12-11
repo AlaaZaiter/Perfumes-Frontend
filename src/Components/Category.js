fc164fa41500665e884099fd3d5553893e647f4a
Mohammad Rahal <rahalmohamad21@gmail.com>
2023-12-11 10:43:55 +0200

    working

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,72 +1,137 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+  import React, { useState, useEffect } from "react";
+  import axios from "axios";
+import '../ComponentCSS/bestsaller.css'
+  const Category = () => {
+    const [categories, setCategories] = useState([]);
+    const [perfumes, setPerfumes] = useState([]);
+    const [cart, setCart] = useState([]) 
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [error, setError] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedPerfume, setSelectedPerfume] = useState(null);
+    const [price, setPrice] = useState(0);
+const [name, setName] = useState("")
+    useEffect(() => {
+      fetchAllPerfumes();
+    }, [selectedCategory]);
 
-const Category = () => {
-  const [categories, setCategories] = useState([]);
-  const [perfumes, setPerfumes] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [error, setError] = useState(null);
-const [category, setCategory] = useState('')
-  useEffect(() => {
-    fetchCategories(category);
-    fetchAllPerfumes();
-  }, [category]);
+    const fetchAllPerfumes = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_URL}/perfume/getAllPerfumes`
+        );
 
-  const fetchCategories = (category) => {
-    console.log((`${process.env.REACT_APP_URL}/perfume/categories/${category}`))
-    axios.get(`${process.env.REACT_APP_URL}/perfume/categories/${category}`)
-      .then((response) => {
-        console.log("catogery" , response);
-        setCategories(response.data.data);
-      })
-      .catch((error) => {
-        setError(error);
-        console.error(error);
-      });
-  };
-
-  const fetchAllPerfumes = () => {
-    axios.get(`${process.env.REACT_APP_URL}/perfume/getAllPerfumes`)
-      .then((response) => {
         setPerfumes(response.data.data);
-        // console.log(response.data.data);
-      })
-      .catch((error) => {
+      } catch (error) {
         setError(error);
         console.error(error);
-      });
-  };
+      }
+    };
 
-  const handleSelectChange = (event) => {
-    const selectedCategory = event.target.value;
-    setCategory(selectedCategory);
-    fetchAllPerfumes(selectedCategory);
-  };
+    useEffect(() => {
+      const extractedCategories = perfumes.reduce((acc, perfume) => {
+        acc.push(...perfume.category);
+        return acc;
+      }, []);
 
-  return (
-    <div>
-      <h2>Categories</h2>
-      <select value={selectedCategory} onChange={handleSelectChange}>
-        <option value="">Select a category</option>
-        {categories.map((category) => (
-          <optgroup key={category._id} value={category.category}>
-             <option>{category.category}</option>  
-           {category.name}
-          </optgroup>
-        ))}
-      </select>
+      const uniqueCategories = Array.from(new Set(extractedCategories));
+      setCategories(uniqueCategories);
+    }, [perfumes]);
 
-      <h2>Perfumes</h2>
-      <ul>
-        {perfumes.map((perfume) => (
-          <div key={perfume._id}>
-            <h1>{perfume.name}</h1>
-            <h1>{perfume.description}</h1>
+    const handleSelectChange = async (event) => {
+      const selectedCategory = event.target.value;
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_URL}/perfume/categories/${selectedCategory}`
+        );
+        const filteredPerfumes = response.data.data.map((perfume) => ({
+          ...perfume,
+          category: [selectedCategory],
+        }));
+        setPerfumes(filteredPerfumes);
+      } catch (error) {
+        setError(error);
+        console.error(error);
+      }
+    };
+
+
+    const addToCart= async () => {
+      try {
+        const response = await axios.post(`${process.env.REACT_APP_URL}/cart/add`);
+        setCart(response.data); 
+      } catch (error) {
+        console.log('There was an error fetching the cart', error);
+      }
+    };
+  
+
+    const openModal = (Perfumes) => {
+    setSelectedPerfume(Perfumes);
+      setShowModal(!showModal);
+    };
+  
+   
+    return (
+      <div>
+        <h2>Categories</h2>
+        <select value={selectedCategory} onChange={handleSelectChange}>
+          <option value="">Select a category</option>
+
+          {categories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+
+        <h2>Perfumes</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 justify-center ">
+          {perfumes.map((perfume) => (
+            <div key={perfume.id}>
+              {perfume.image && (
+                <img
+                  src={perfume.image}
+                  className="p-8 rounded-t-lg"
+                  alt="product image"
+                />
+              )}
+              <div className="px-5 pb-5">
+                <h5 className="text-xl font-semibold tracking-tight text-center dark:text-black">
+                  {perfume.name}
+                </h5>
+                <div className="flex items-center justify-between">
+                  <button
+                    class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    type="button"
+                    onClick={openModal}
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
             </div>
-        ))}
-      </ul>
-    </div>
-  );
-};
+          ))}
+        </div>
+        {showModal && (
+          <div className="modal">
+            <div className="modal-content">
+            <span className="close"  
+            onClick={() => {
+                setShowModal(false);
+              }}>
+              &times;
+            </span>
+            
+            {perfumes.id}
+            <button onClick={() => addToCart(selectedPerfume)}>
+              Check Out
+            </button>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  };
 
-export default Category;
+  export default Category;
