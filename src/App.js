5987f4c17cb01dbfc9ac065d3a689711bed59346
fatima <fatimakarout1717@gmail.com>
2023-12-12 16:43:15 +0200

    without adim

--- src/App.js
+++ src/App.js
@@ -3,17 +3,45 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './Components/HomePage'
 import Contact from './Components/Contact';
 import SellerDashboard from './Components/SellerDashboard';
-import AdminDashboard from './Components/AdminDashboard'
+import AdminDashboard from './Components/AdminDashboard';
+import ProtectRoute from "./Components/ProtectRoute";
+import Login  from './Pages/Login'
+import Register from './Pages/Register';
 function App() {
   return (
     <div>
 <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/seller" element={<SellerDashboard />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register/>} />
         <Route path="/contact" element={<Contact />} />
+        <Route
+          path="/"
+          element={
+            <ProtectRoute adminOnly={false}>
+           <HomePage/>
+            </ProtectRoute>
+          }
+          
+        />
+         <Route
+          path="/admin"
+          element={
+            <ProtectRoute adminOnly={true}>
+              <AdminDashboard />
+            </ProtectRoute>
+          } 
+        />
+          <Route
+          path="/seller"
+          element={
+            <ProtectRoute sellerOnly={true}>
+              <SellerDashboard />
+            </ProtectRoute>
+          }
+          />
+        {/* here you can add fall back to 404 page for example */}
+        
       </Routes>
     </BrowserRouter>   
      </div>
