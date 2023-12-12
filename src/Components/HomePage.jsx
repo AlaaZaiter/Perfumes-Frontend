97c1a4b0e946dd45efedf69e17aadc094b4c8083
AlaaZaiter2 <zaiteralaa29@gmail.com>
2023-12-12 12:40:18 +0200

    saved changes

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -4,7 +4,8 @@ import Category from "./Category";
 import BestSaller from "./BestSaller.jsx";
 import "../ComponentCSS/HomePageContainer.css";
 import React, { createContext, useContext } from 'react';
-import { BrowserRouter as Router, Route ,Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route ,Routes} from 'react-router-dom'; 
+import FooterAndOurClients from "./FooterAndOurClients.jsx";
 
 
 function HomePage() {
@@ -13,6 +14,7 @@ function HomePage() {
         <Header/>
         <Category/>
         <BestSaller/>
+        <FooterAndOurClients/>
         </div>
     )}
     export default HomePage;
