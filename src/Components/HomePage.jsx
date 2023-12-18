024c0235703dcf46119b9969b267640fdadd4149
AlaaZaiter2 <zaiteralaa29@gmail.com>
2023-12-18 09:50:01 +0200

    aved changes

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,7 +1,6 @@
 
 import Header from "./Header";
 import Category from "./Category";
-import BestSaller from "./BestSaller.jsx";
 import "../ComponentCSS/HomePageContainer.css";
 import React, { createContext, useContext } from 'react';
 import { BrowserRouter as Router, Route ,Routes} from 'react-router-dom'; 
@@ -13,7 +12,6 @@ function HomePage() {
       <div className="HomePageContainer">
         <Header/>
         <Category/>
-        <BestSaller/>
         <FooterAndOurClients/>
         </div>
     )}
